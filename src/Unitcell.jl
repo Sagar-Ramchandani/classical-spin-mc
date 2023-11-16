b37be201205edfd43968f12d38377f26d14494ed
Sagar Ramchandani <sramchan@thp.uni-koeln.de>
2023-11-16 13:27:54 +0100

    Better docs and more helper functions

--- src/Unitcell.jl
+++ src/Unitcell.jl
@@ -8,6 +8,12 @@ struct UnitCell{D}
     interactionsField::Vector{SVector{3,Float64}}
 end
 
+"""
+--------------------------------------------------------------------------------
+Constructor functions
+--------------------------------------------------------------------------------
+"""
+
 #Constructor that accepts D number of SVectors as input to use as primitives. 
 function UnitCell(primitive::Vararg{SVector{D,Float64},D}) where {D}
     return UnitCell{D}(primitive, Vector{SVector{D,Float64}}(undef, 0), Vector{Tuple{Pair{Int,Int},NTuple{D,Int},SMatrix{3,3,Float64,9}}}(undef, 0), Vector{SMatrix{3,3,Float64,9}}(undef, 0), Vector{SVector{3,Float64}}(undef, 0))
@@ -18,9 +24,11 @@ function UnitCell(primitive::Vararg{NTuple{D,Float64},D}) where {D}
     return UnitCell(SVector.(primitive)...)
 end
 
-function Base.:show(io::IO, uc::UnitCell{D}) where {D}
-    println(io, "$(2)D Unitcell with $(length(uc.basis)) sites and $(length(uc.interactions)) interactions")
-end
+"""
+--------------------------------------------------------------------------------
+addInteraction! functions
+--------------------------------------------------------------------------------
+"""
 
 """
 Adds an interaction between spin1 located at basis site `b1` of the given `unitcell` and spin2 at basis site `b2` in a unit cell that is offset by `offset` lattice vectors. 
@@ -47,6 +55,12 @@ function addInteraction!(unitcell::UnitCell{D}, b1::Int, b2s::Vector{Int}, M::SM
 end
 
 """
+--------------------------------------------------------------------------------
+setInteractionOnSite! functions
+--------------------------------------------------------------------------------
+"""
+
+"""
 Sets the self-interaction for a spin located at basis site 'b' of the given 'unitcell'. 
 The exchange energy is calculated as spin'.M.spin
 """
@@ -55,6 +69,17 @@ function setInteractionOnsite!(unitcell::UnitCell{D}, b::Int, M::SMatrix{3,3,Flo
     return nothing
 end
 
+function setInteractionOnsite!(unitcell::UnitCell{D}, b::Int, M::Vector{SMatrix{3,3,Float64,9}}) where {D}
+    unitcell.interactionsOnsite = M
+    return nothing
+end
+
+"""
+--------------------------------------------------------------------------------
+setField! functions
+--------------------------------------------------------------------------------
+"""
+
 """
 Sets the magnetic field for a spin at the basis site 'b'.
 """
@@ -62,6 +87,17 @@ function setField!(unitcell::UnitCell{D}, b::Int, B::SVector{3,Float64}) where {
     unitcell.interactionsField[b] = B
     return nothing
 end
+
+function setField!(unitcell::UnitCell{D}, b::Int, B::Vector{SVector{3,Float64}}) where {D}
+    unitcell.interactionsField = B
+    return nothing
+end
+
+"""
+--------------------------------------------------------------------------------
+Basis functions
+--------------------------------------------------------------------------------
+"""
 
 """
 Adds a new basis site to the given 'unitcell' at the given position with no self-interaction and no magnetic field.
@@ -88,6 +124,7 @@ function addBasisSite!(unitcell::UnitCell{D}, positions::Vector{NTuple{D,Float64
     for position in positions
         addBasisSite!(unitcell, SVector(position))
     end
+    return nothing
 end
 
 function resetBasis!(unitcell::UnitCell{D}) where {D}
@@ -95,6 +132,16 @@ function resetBasis!(unitcell::UnitCell{D}) where {D}
     return nothing
 end
 
+"""
+--------------------------------------------------------------------------------
+Extend Base
+--------------------------------------------------------------------------------
+"""
+
 function Base.length(unitcell::UnitCell{D})::Int where {D}
     return length(unitcell.basis)
+end
+
+function Base.:show(io::IO, uc::UnitCell{D}) where {D}
+    println(io, "$(2)D Unitcell with $(length(uc.basis)) sites and $(length(uc.interactions)) interactions")
 end
