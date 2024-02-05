33c892dcd78a77f8d9352b57207af4084ce03168
Sagar Ramchandani <sramchan@thp.uni-koeln.de>
2024-02-05 15:20:38 +0100

    Simplifying Observables

--- src/Observables.jl
+++ src/Observables.jl
@@ -15,62 +15,94 @@ const MatrixObs = Union{BinnedMatrixObs,FullMatrixObs}
 
 abstract type AbstractObservables end
 
+import Base: ==
+function ==(a::T, b::T) where {T<:AbstractObservables}
+    fields = fieldnames(T)
+    status = true
+    for field in fields
+        status = status && (getfield(a, field) == getfield(b, field))
+    end
+    return status
+end
+
 mutable struct Observables <: AbstractObservables
-    """
-    Add fraction as a proper field and 
-    possibly move labels somewhere else
-    """
     energy::ErrorPropagator{Float64,32}
-
+    specificHeat::Tuple{Float64,Float64}
     magnetization::BinnedObs
-    squaredMagnetization::BinnedObs
-    quadMagnetization::BinnedObs
-    mPlanar::BinnedObs
-    z6::BinnedObs
-
     magnetizationVector::VectorObs
-    magnetizationVectorPerSite::MatrixObs
-
     correlation::BinnedMatrixObs
-
-    chirality::Tuple{Vector{Vector{Int64}},BinnedObs}
-
-    """
-    Possibly change them to FullBinners or LogBinners
-    """
     labels::Vector{Int64}
     replicaAcceptance::Vector{Float64}
 end
+
+#mutable struct Observables <: AbstractObservables
+#"""
+#Add fraction as a proper field and 
+#possibly move labels somewhere else
+#"""
+#energy::ErrorPropagator{Float64,32}
+
+#magnetization::BinnedObs
+##squaredMagnetization::BinnedObs
+##quadMagnetization::BinnedObs
+##mPlanar::BinnedObs
+##z6::BinnedObs
+
+#magnetizationVector::VectorObs
+##magnetizationVectorPerSite::MatrixObs
+
+##correlation::BinnedMatrixObs
+
+##chirality::Tuple{Vector{Vector{Int64}},BinnedObs}
+
+#"""
+#Possibly change them to FullBinners or LogBinners
+#"""
+#labels::Vector{Int64}
+#replicaAcceptance::Vector{Float64}
+#end
 
 function Base.:show(io::IO, obs::Observables)
     println(io, "Observables object with $(length(fieldnames(typeof(obs)))) observables")
 end
 
-function Observables(lattice::T, storeAll::Bool) where {T<:Lattice}
+function Observables(lattice::T) where {T<:Lattice}
     return Observables(
         ErrorPropagator(Float64),
+        (0.0, 0.0),
         LogBinner(Float64),
-        LogBinner(Float64),
-        LogBinner(Float64),
-        LogBinner(Float64),
-        LogBinner(Float64),
-        storeAll ? FullBinner([zeros(Float64, 3)]) : LogBinner(zeros(Float64, 3)),
-        storeAll ? FullBinner([zeros(Float64, 3, length(lattice.unitcell))]) : LogBinner(zeros(Float64, 3, length(lattice.unitcell))),
+        LogBinner(zeros(Float64, 3)),
         LogBinner(zeros(Float64, lattice.length, length(lattice.unitcell))),
-        (calcTriangles(lattice), LogBinner(Float64)),
         Int64[],
         Float64[],
     )
 end
 
+#function Observables(lattice::T, storeAll::Bool) where {T<:Lattice}
+#return Observables(
+#ErrorPropagator(Float64),
+#LogBinner(Float64),
+##LogBinner(Float64),
+##LogBinner(Float64),
+##LogBinner(Float64),
+##LogBinner(Float64),
+#storeAll ? FullBinner([zeros(Float64, 3)]) : LogBinner(zeros(Float64, 3)),
+##storeAll ? FullBinner([zeros(Float64, 3, length(lattice.unitcell))]) : LogBinner(zeros(Float64, 3, length(lattice.unitcell))),
+##LogBinner(zeros(Float64, lattice.length, length(lattice.unitcell))),
+##(calcTriangles(lattice), LogBinner(Float64)),
+#Int64[],
+#Float64[],
+#)
+#end
+
 #Review this
-function getSpecificHeat(obs::Observables, beta::Float64, N::Int64)
-    c(e) = beta * beta * (e[2] - e[1] * e[1]) * N
-    ∇c(e) = [-2.0 * beta * beta * e[1] * N, beta * beta * N]
+function getSpecificHeat(obs::Observables, β::Float64, N::Int64)
+    k = β * β * N
+    c(e) = k * (e[2] - e[1] * e[1])
+    ∇c(e) = [-2.0 * e[1] * k, k]
     heat = mean(obs.energy, c)
-    dheat = sqrt(abs(var(
-        obs.energy, ∇c, BinningAnalysis._reliable_level(obs.energy))) /
-                 obs.energy.count[BinningAnalysis._reliable_level(obs.energy)])
+    reliableEnergyLevel = BinningAnalysis._reliable_level(obs.energy)
+    dheat = sqrt(abs(var(obs.energy, ∇c, reliableEnergyLevel)) / obs.energy.count[reliableEnergyLevel])
     return (heat, dheat)
 end
 
@@ -89,18 +121,16 @@ function getFraction(vec)
 end
 
 #Method for generic computation of observables
-function performMeasurements!(obs::O, lattice::T, energy::Float64) where {O<:AbstractObservables,T<:Lattice}
-    fields = fieldnames(O)
-    for field in fields
-        push!(getfield(obs, field), getfield(Main, Symbol("get" * String(field)))(lattice))
-    end
-    return nothing
-end
+#function performMeasurements!(obs::O, lattice::T, energy::Float64) where {O<:AbstractObservables,T<:Lattice}
+#fields = fieldnames(O)
+#for field in fields
+#push!(getfield(obs, field), getfield(Main, Symbol("get" * String(field)))(lattice))
+#end
+#return nothing
+#end
 
-#Specialized method for built-in observables type
 function performMeasurements!(observables::Observables, lattice::T, energy::Float64) where {T<:Lattice}
     #measure energy and energy^2
-    siteList = first(observables.chirality)
     push!(observables.energy, energy / length(lattice), energy * energy / (length(lattice) * length(lattice)))
 
     m = getMagnetization(lattice)
@@ -108,20 +138,39 @@ function performMeasurements!(observables::Observables, lattice::T, energy::Floa
     push!(observables.magnetizationVector, m)
     push!(observables.magnetization, mMagnitude)
 
-    #Measuring m^2
-    m2 = mMagnitude^2
-    push!(observables.squaredMagnetization, m2)
-    #Measuring m^4
-    m4 = mMagnitude^4
-    push!(observables.quadMagnetization, m4)
-
-    #Measuring planar magnetization and z6
-    #mPlanar, z6 = getZ6(lattice)
-    #push!(observables.z6, z6)
-    #push!(observables.mPlanar, mPlanar)
-
-    #measure spin correlations
+    #measuring correlation
     push!(observables.correlation, getCorrelation(lattice))
-    push!(observables.magnetizationVectorPerSite, getMagnetizationPerSite(lattice))
-    push!(last(observables.chirality), getChirality(lattice, siteList))
+end
+
+##Specialized method for built-in observables type
+#function performMeasurements!(observables::Observables, lattice::T, energy::Float64) where {T<:Lattice}
+##measure energy and energy^2
+##siteList = first(observables.chirality)
+#push!(observables.energy, energy / length(lattice), energy * energy / (length(lattice) * length(lattice)))
+
+#m = getMagnetization(lattice)
+#mMagnitude = norm(m)
+#push!(observables.magnetizationVector, m)
+#push!(observables.magnetization, mMagnitude)
+
+##Measuring m^2
+##m2 = mMagnitude^2
+##push!(observables.squaredMagnetization, m2)
+##Measuring m^4
+##m4 = mMagnitude^4
+##push!(observables.quadMagnetization, m4)
+
+##Measuring planar magnetization and z6
+##mPlanar, z6 = getZ6(lattice)
+##push!(observables.z6, z6)
+##push!(observables.mPlanar, mPlanar)
+
+##measure spin correlations
+##push!(observables.correlation, getCorrelation(lattice))
+##push!(observables.magnetizationVectorPerSite, getMagnetizationPerSite(lattice))
+##push!(last(observables.chirality), getChirality(lattice, siteList))
+#end
+
+function performPostMeasurements!(observables::Observables, lattice::T, β::Float64) where {T<:Lattice}
+    observables.specificHeat = getSpecificHeat(observables, β, size(lattice))
 end
