04674a07bf3760c4ea61cb9e582f5a06537e29ee
Sagar Ramchandani <sramchan@thp.uni-koeln.de>
2024-03-11 15:46:43 +0100

    Added Integration tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,8 +2,10 @@ using ClassicalSpinMC
 using HDF5
 using Random
 using Test
+
 include("UnitCellTests.jl")
 include("LatticeTests.jl")
 include("ObservablesTests.jl")
 include("MonteCarloTests.jl")
-#include("SpinTests.jl")
+include("SpinTests.jl")
+include("IntegrationTest.jl")
