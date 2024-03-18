318bf46d9e738fd29f6fa6a6130d7e846199bc10
Sagar Ramchandani <sramchan@thp.uni-koeln.de>
2024-03-18 17:51:20 +0100

    Additional exports for observables extension

--- src/ClassicalSpinMC.jl
+++ src/ClassicalSpinMC.jl
@@ -2,40 +2,44 @@ module ClassicalSpinMC
 
 include("Unitcell.jl")
 export UnitCell, dimension, addInteraction!, setInteractionOnsite!, setField!, addBasisSite!, addAnisotropy!,
-	resetBasis!
+    resetBasis!
 
 include("Lattice.jl")
 export Lattice, size, length, getSpin, setSpin!,
-	getSitePosition, getInteractionSites, getInteractionMatrices,
-	getInteractionOnsite, getInteractionField
+    getSitePosition, getInteractionSites, getInteractionMatrices,
+    getInteractionOnsite, getInteractionField
 
 include("Observables.jl")
-export AbstractObservables, Observables, performMeasurements!
+#Export constants
+export BinnedObs, FullObs, BinnedVectorObs, FullVectorObs, VectorObs, BinnedMatrixObs, FullMatrixObs, MatrixObs
+
+#Export Observables
+export getSpecificHeat, AbstractObservables, Observables, performMeasurements!, performPostMeasurements!
 
 include("Spin.jl")
 export marsagliaSphereUpdate, sphericalUpdate, conicalUpdate,
-	getEnergy, getEnergyDifference, getMagnetization, getMagnetizationPerSite, getCorrelation,
-	calcTriangles
+    getEnergy, getEnergyDifference, getMagnetization, getMagnetizationPerSite, getCorrelation,
+    calcTriangles
 
 include("MonteCarlo.jl")
 export MonteCarlo, MonteCarloStatistics, MonteCarloParameters, MonteCarloAnnealing, MonteCarloExchange,
-	run!, anneal, initSpinConfiguration!, localSweep, microcanonicalSweep!, replicaExchange!,
-	localUpdate, microcanonicalRotation, microcanonicalRotationRandom, printStatistics!, sanityChecks,
-	createChannels
+    run!, anneal, initSpinConfiguration!, localSweep, microcanonicalSweep!, replicaExchange!,
+    localUpdate, microcanonicalRotation, microcanonicalRotationRandom, printStatistics!, sanityChecks,
+    createChannels
 
 include("IO.jl")
 export writeUnitcell!, readUnitcell,
-	writeLattice!, readLattice,
-	writeMonteCarloParameters!, readMonteCarloParameters,
-	writeMonteCarloStatistics!, readMonteCarloStatistics,
-	writeObservables!, readObservables,
-	writeMonteCarlo!, readMonteCarlo,
-	save!, load #Only export for debugging
+    writeLattice!, readLattice,
+    writeMonteCarloParameters!, readMonteCarloParameters,
+    writeMonteCarloStatistics!, readMonteCarloStatistics,
+    writeObservables!, readObservables,
+    writeMonteCarlo!, readMonteCarlo,
+    save!, load #Only export for debugging
 
 include("Plotting.jl")
 export loadObservables, getSpins, groupSpins,
-	plotSpins, plotSpins!, originPlot, originPlot!, gsPlot, gsPlot!, gsMultiple,
-	plotObservables, plotMC
+    plotSpins, plotSpins!, originPlot, originPlot!, gsPlot, gsPlot!, gsMultiple,
+    plotObservables, plotMC
 
 using Reexport
 @reexport using BinningAnalysis
