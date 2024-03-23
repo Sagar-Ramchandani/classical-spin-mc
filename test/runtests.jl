0609f8e6813e5342b27accd1abd160915a138b5e
Sagar Ramchandani <sramchan@thp.uni-koeln.de>
2024-03-23 16:02:31 +0100

    Added LinearAlgebra for testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,12 +3,13 @@ using HDF5
 using Random
 using Test
 using Suppressor
+using LinearAlgebra
 
 #include("UnitCellTests.jl")
 #include("LatticeTests.jl")
 #include("ObservablesTests.jl")
 #include("MonteCarloTests.jl")
-#include("SpinTests.jl")
+include("SpinTests.jl")
 #include("IntegrationTest.jl")
 #include("AnnealTests.jl")
-include("PTTests.jl")
+#include("PTTests.jl")
