10d4381f2746e7d10ed61909bb0f3daf2f3e55a8
Sagar Ramchandani <sramchan@thp.uni-koeln.de>
2024-07-22 12:38:32 +0200

    Add keys

--- docs/make.jl
+++ docs/make.jl
@@ -2,10 +2,3 @@ using Documenter, ClassicalSpinMC
 
 makedocs(sitename = "ClassicalSpinMC")
 deploydocs(repo="github.com/Sagar-Ramchandani/classical-spin-mc.git")
-
-# Documenter can also automatically deploy documentation to gh-pages.
-# See "Hosting Documentation" and deploydocs() in the Documenter manual
-# for more information.
-#=deploydocs(
-    repo = "<repository url>"
-)=#
